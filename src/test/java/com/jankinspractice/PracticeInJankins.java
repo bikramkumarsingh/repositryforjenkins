66752a338bddada9a5193eba2f887159da5decca
bikramkumarsingh <true.bikram123@gmail.com>
2022-12-20 15:13:13 +0530

    pushing to github

---
+++ src/test/java/com/jankinspractice/PracticeInJankins.java
@@ -0,0 +1,11 @@
+package com.jankinspractice;
+
+import org.testng.annotations.Test;
+
+public class PracticeInJankins {
+
+	@Test
+	public void printing() {
+		System.out.println("jenkins ");
+	}
+}
